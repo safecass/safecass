a971b4d01cf5d3cf63426459586a5baaa92eee0f
Min Yang Jung <myj@jhu.edu>
2012-07-01 21:42:38 +0000

    Min: safety framework

--- examples/latency/main.cpp
+++ examples/latency/main.cpp
@@ -10,11 +10,34 @@
 
 */
 
-#include <stdio>
+#include <iostream>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "hello" << std::endl;
+    if (argc != 3) {
+        std::cout << "usage: latency period duration\n";
+        std::cout << "       period  : thread period in msec\n";
+        std::cout << "       duration: test duration in seconds\n";
+        return 1;
+    }
+
+    const double period = atof(argv[1]);
+    const double duration = atof(argv[2]);
+
+    std::cout << "period: " << period << ", duration: " << duration << std::endl;
+
+    // TODO: thread creation
+
+    // TODO: FDD setup (or monitor)
+
+    // TODO: start experiment
+
+    // TODO: collect data during experiment
+
+    // TODO: (visualization setup?)
+
+    // TODO: post-processing, statistics
 
     return 0;
 }
