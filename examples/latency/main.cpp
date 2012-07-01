65e34b2cae4ca5082835561c82b630a969e76ff1
Min Yang Jung <myj@jhu.edu>
2012-07-01 21:04:29 +0000

    Min: initial structures for Safety Framework

---
+++ examples/latency/main.cpp
@@ -0,0 +1,20 @@
+/*
+
+  Safety Framework for Component-based Robotics
+
+  Copyright (C) 2012 Min Yang Jung <myj@jhu.edu>
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or copy at
+  http://www.boost.org/LICENSE_1_0.txt)
+
+*/
+
+#include <stdio>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello" << std::endl;
+
+    return 0;
+}
