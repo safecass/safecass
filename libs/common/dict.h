1c900bc1a025058cdb168c197469e7f22b1b4b69
Min Yang Jung <myj@jhu.edu>
2013-05-21 19:28:39 -0400

    libs/common: Updated SF JSON dictionary

--- libs/common/dict.h
+++ libs/common/dict.h
@@ -32,23 +32,35 @@ namespace SF {
         namespace Json {
             DEFINE_KEYWORD(component);
             DEFINE_KEYWORD(config);
+            DEFINE_KEYWORD(command);
+            DEFINE_KEYWORD(custom);
             DEFINE_KEYWORD(data);
             DEFINE_KEYWORD(detector);
             DEFINE_KEYWORD(dutycycle_total);
             DEFINE_KEYWORD(dutycycle_total_ratio);
             DEFINE_KEYWORD(dutycycle_user);
             DEFINE_KEYWORD(dutycycle_user_ratio);
+            DEFINE_KEYWORD(event);
+            DEFINE_KEYWORD(event_generator);
+            DEFINE_KEYWORD(event_handler);
             DEFINE_KEYWORD(fault);
             DEFINE_KEYWORD(fault_application);
+            DEFINE_KEYWORD(filter_event); // MJTEMP: am I using this?
+            DEFINE_KEYWORD(function);
             DEFINE_KEYWORD(identity);
             DEFINE_KEYWORD(interface_provided);
             DEFINE_KEYWORD(interface_required);
+            DEFINE_KEYWORD(invalid);
             DEFINE_KEYWORD(localization);
             DEFINE_KEYWORD(location);
             DEFINE_KEYWORD(MARGIN);
             DEFINE_KEYWORD(monitor);
             DEFINE_KEYWORD(name);
-            DEFINE_KEYWORD(OUTPUT);
+            DEFINE_KEYWORD(off);
+            DEFINE_KEYWORD(on);
+            DEFINE_KEYWORD(output);
+            DEFINE_KEYWORD(output_type);
+            DEFINE_KEYWORD(period);
             DEFINE_KEYWORD(period_actual);
             DEFINE_KEYWORD(period_nominal);
             DEFINE_KEYWORD(process);
@@ -56,6 +68,7 @@ namespace SF {
             DEFINE_KEYWORD(sampling_rate);
             DEFINE_KEYWORD(severity);
             DEFINE_KEYWORD(state);
+            DEFINE_KEYWORD(stream);
             DEFINE_KEYWORD(SUBSCRIBE);
             DEFINE_KEYWORD(THRESHOLD);
             DEFINE_KEYWORD(thread_period);
