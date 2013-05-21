e6706be5e9aab3edff14100bd7160880e8f05645
Min Yang Jung <myj@jhu.edu>
2013-05-21 19:26:59 -0400

    libs/monitor: Updated monitor

--- libs/monitor/monitor.cpp
+++ libs/monitor/monitor.cpp
@@ -4,7 +4,7 @@
 
   Created on: July 7, 2012
 
-  Copyright (C) 2012 Min Yang Jung, Peter Kazanzides
+  Copyright (C) 2012-2013 Min Yang Jung, Peter Kazanzides
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE_1_0.txt or copy at
@@ -15,6 +15,7 @@
 #include "monitor.h"
 #include "dict.h"
 #include "json.h"
+#include "utils.h"
 
 namespace SF {
 
@@ -26,8 +27,8 @@ using namespace Dict;
 Monitor::Monitor() 
     : UID(++UIDCounter), // UID=0 means invalid monitor target
       Target(TARGET_INVALID), LocationID(0), State(STATE_INVALID),
-      Output(OUTPUT_INVALID), SamplingRate(0), LastSamplingTick(0),
-      AttachedToActiveFilter(false)
+      Output(OUTPUT_INVALID), SamplingRate(0), 
+      LastSamplingTick(0), AttachedToActiveFilter(false)
 {
     Initialize();
 }
@@ -38,11 +39,34 @@ Monitor::Monitor(const TargetType target,
                  const OutputType output,
                  const SamplingRateType samplingRate,
                  double lastSamplingTick)
-    : Target(target), LocationID(locationId), State(state),
-      Output(output), SamplingRate(samplingRate), LastSamplingTick(lastSamplingTick),
-      AttachedToActiveFilter(false)
+    : UID(++UIDCounter),
+      Target(target), LocationID(locationId), State(state),
+      Output(output), SamplingRate(samplingRate), 
+      LastSamplingTick(lastSamplingTick), AttachedToActiveFilter(false)
 {
     Initialize();
+}
+
+Monitor::Monitor(const JSON::JSONVALUE & jsonNode)
+    : UID(++UIDCounter)
+{
+    Initialize();
+
+    // Extract and set common (base) fields
+    this->Target       = GetTargetTypeFromString(JSON::GetSafeValueString(jsonNode, Json::type));
+    this->State        = GetStateTypeFromString(JSON::GetSafeValueString(jsonNode, Json::state));
+    this->Output       = GetOutputTypeFromString(JSON::GetSafeValueString(jsonNode, Json::output_type));
+    this->SamplingRate = JSON::GetSafeValueUInt(jsonNode, Json::sampling_rate);
+
+    // Extract target information
+    const JSON::JSONVALUE location(jsonNode[Json::location]);
+    const std::string processName           = JSON::GetSafeValueString(location, Json::process);
+    const std::string componentName         = JSON::GetSafeValueString(location, Json::component);
+    const std::string interfaceProvidedName = JSON::GetSafeValueString(location, Json::interface_provided);
+    const std::string interfaceRequiredName = JSON::GetSafeValueString(location, Json::interface_required);
+
+    this->LocationID = new EventLocationBase(processName, componentName,
+                                             interfaceProvidedName, interfaceRequiredName);
 }
 
 Monitor::~Monitor()
@@ -124,11 +148,14 @@ const std::string Monitor::GetTargetTypeString(const TargetType type)
 
 Monitor::TargetType Monitor::GetTargetTypeFromString(const std::string & str)
 {
-    if (str.compare(STR_TARGET_CUSTOM) == 0)                 return TARGET_CUSTOM;
-    if (str.compare(STR_TARGET_THREAD_PERIOD) == 0)          return TARGET_THREAD_PERIOD;
-    if (str.compare(STR_TARGET_THREAD_DUTYCYCLE_USER) == 0)  return TARGET_THREAD_DUTYCYCLE_USER;
-    if (str.compare(STR_TARGET_THREAD_DUTYCYCLE_TOTAL) == 0) return TARGET_THREAD_DUTYCYCLE_TOTAL;
-    if (str.compare(STR_TARGET_FILTER_EVENT) == 0)           return TARGET_FILTER_EVENT;
+    std::string _str(str);
+    to_uppercase(_str);
+
+    if (_str.compare(STR_TARGET_CUSTOM) == 0)                 return TARGET_CUSTOM;
+    if (_str.compare(STR_TARGET_THREAD_PERIOD) == 0)          return TARGET_THREAD_PERIOD;
+    if (_str.compare(STR_TARGET_THREAD_DUTYCYCLE_USER) == 0)  return TARGET_THREAD_DUTYCYCLE_USER;
+    if (_str.compare(STR_TARGET_THREAD_DUTYCYCLE_TOTAL) == 0) return TARGET_THREAD_DUTYCYCLE_TOTAL;
+    if (_str.compare(STR_TARGET_FILTER_EVENT) == 0)           return TARGET_FILTER_EVENT;
 
     return TARGET_INVALID;
 }
@@ -143,8 +170,11 @@ const std::string Monitor::GetStateTypeString(const StateType type)
 
 Monitor::StateType Monitor::GetStateTypeFromString(const std::string & str)
 {
-    if (str.compare(Dict::OFF) == 0) return STATE_OFF;
-    if (str.compare(Dict::ON) == 0) return STATE_ON;
+    std::string _str(str);
+    to_uppercase(_str);
+
+    if (_str.compare(Dict::OFF) == 0) return STATE_OFF;
+    if (_str.compare(Dict::ON) == 0) return STATE_ON;
 
     return STATE_INVALID;
 }
@@ -159,21 +189,33 @@ const std::string Monitor::GetOutputTypeString(OutputType type)
 
 Monitor::OutputType Monitor::GetOutputTypeFromString(const std::string & str)
 {
-    if (str.compare(Dict::STREAM) == 0) return OUTPUT_STREAM;
-    if (str.compare(Dict::EVENT) == 0) return OUTPUT_EVENT;
+    std::string _str(str);
+    to_uppercase(_str);
+
+    if (_str.compare(Dict::STREAM) == 0) return OUTPUT_STREAM;
+    if (_str.compare(Dict::EVENT) == 0) return OUTPUT_EVENT;
 
     return OUTPUT_INVALID;
 }
 
-void Monitor::ToStream(std::ostream & outputStream) const
+void Monitor::ToStream(std::ostream & outputStream, bool includeLocation) const
 {
-    outputStream << "Target type: " << GetTargetTypeString(Target) << ", "
-                 << "LocationID: " << *LocationID << ", "
-                 << "State: " << GetStateTypeString(State) << ", "
+    outputStream << "Target type: " << GetTargetTypeString(Target) << ", ";
+    if (includeLocation) {
+        outputStream << "LocationID: ";
+        if (LocationID == 0)
+            outputStream << "NULL";
+        else
+            outputStream << *LocationID;
+        outputStream << ", ";
+    }
+    outputStream << "State: " << GetStateTypeString(State) << ", "
                  << "OutputType: " << GetOutputTypeString(Output) << ", "
                  << "SamplingRate: " << SamplingRate << ", "
                  << "Last sampling tick: " << LastSamplingTick << ", "
                  << "Active filter: " << AttachedToActiveFilter;
+    if (!includeLocation)
+        outputStream << ", ";
 }
 
 };
--- libs/monitor/monitor.h
+++ libs/monitor/monitor.h
@@ -22,117 +22,148 @@ namespace SF {
 
 class SFLIB_EXPORT Monitor {
 public:
-    /*! Typedef of numerical representation of unique id of monitor targets */
+    //! Typedef of numerical representation of unique id of monitor targets
     typedef unsigned int UIDType;
     enum { INVALID_UID = 0 };
 
-    /*! Typedef for monitor target (what to monitor) */
+    //! Typedef for monitor target (what to monitor)
+    /*! In JSON format, each row can be specified as
+        TARGET_INVALID                : "invalid"
+        TARGET_THREAD_PERIOD          : "period"
+        TARGET_THREAD_DUTYCYCLE_USER  : "dutycycle_user"
+        TARGET_THREAD_DUTYCYCLE_TOTAL : "dutycycle_total"
+        TARGET_FILTER_EVENT           : "filter_event"
+        TARGET_CUSTOM                 : "custom"
+        \sa SF::Dict::Json (libs/common/dict.h)
+    */
     typedef enum {
-        TARGET_INVALID,
-        // Measure period of periodic task (see FAULT_COMPONENT_PERIOD)
-        TARGET_THREAD_PERIOD,
-        // Measure duty cycle of periodic task (see FAULT_COMPONENT_OVERRUN)
-        TARGET_THREAD_DUTYCYCLE_USER,
-        TARGET_THREAD_DUTYCYCLE_TOTAL,
-        // Events from filter (usually faults)
-        TARGET_FILTER_EVENT,
-        // Custom event (defined by users)
-        TARGET_CUSTOM
+        TARGET_INVALID,                /*!< initial value (invalid) */
+        TARGET_THREAD_PERIOD,          /*!< period of periodic task (see FAULT_COMPONENT_PERIOD) */
+        TARGET_THREAD_DUTYCYCLE_USER,  /*!< duty cycle of user thread (see FAULT_COMPONENT_OVERRUN) */
+        TARGET_THREAD_DUTYCYCLE_TOTAL, /*!< duty cycle of thread (see FAULT_COMPONENT_OVERRUN) */
+        TARGET_FILTER_EVENT,           /*!< MJTEMP: events from filter (usually faults) */
+        TARGET_CUSTOM                  /*!< custom monitoring target */
         // [SFUPDATE]
     } TargetType;
 
-    /*! Typedef for set of monitor targets */
-    typedef unsigned int TargetSetType;
+    //! Typedef for set of monitor targets
+    //typedef unsigned int TargetSetType;
 
-    /*! Typedef for monitor state */
+    //! Typedef for monitor state
+    /*! In JSON format, each row can be specified as
+        STATE_INVALID : "invalid"
+        STATE_OFF     : "off"
+        STATE_ON      : "on"
+        \sa SF::Dict::Json (libs/common/dict.h)
+    */
     typedef enum {
-        STATE_INVALID, 
-        STATE_OFF, 
-        STATE_ON
+        STATE_INVALID, /*!< initial value (invalid) */
+        STATE_OFF,     /*!< monitoring disabled */
+        STATE_ON       /*!< monitoring enabled */
     } StateType;
 
-    /*! Typedef for monitoring types (how to monitor) */
+    //! Typedef for monitoring types (how to monitor)
+    /*! In JSON format, each row can be specified as
+        OUTPUT_INVALID : "invalid"
+        OUTPUT_STREAM  : "stream"
+        OUTPUT_EVENT   : "event"
+        \sa SF::Dict::Json (libs/common/dict.h)
+    */
     typedef enum {
-        OUTPUT_INVALID, 
-        OUTPUT_STREAM,
-        OUTPUT_EVENT 
+        OUTPUT_INVALID, /*!< initial value (invalid) */
+        OUTPUT_STREAM,  /*!< continuous sampling (stream of samples) */
+        OUTPUT_EVENT    /*!< event-based sampling */
     } OutputType;
 
 protected:
-    /*! Numerical representation of unique id */
+    //! Numerical representation of unique id
     UIDType UID;
 
-    /*! Monitoring target */
+    //! Monitoring target
     TargetType Target;
 
-    /*! Target location (declared as pointer to support middleware-specific classes
+    //! Target location 
+    /*! Declared as pointer to support middleware-specific classes
         which derive from SF::EventLocationBase).  An instance of EventLocationBase 
         needs to be created outside of this class and it will be cleaned up by this 
-        class, i.e., the instance should not be deleted outside of this class. */
+        class, i.e., the instance should not be deleted outside of this class.
+    */
     EventLocationBase * LocationID;
 
-    /*! Monitoring state */
+    //! Monitoring state
     StateType State;
 
-    /*! Monitor output type */
+    //! Monitor output type
     OutputType Output;
 
-    /*! Sampling rate (valid only for STREAM-type monitors) */
+    //! Sampling rate (valid only for stream type monitors)
     SamplingRateType SamplingRate;
 
-    /*! Addresses to publish monitoring data */
+    //! Addresses to publish monitoring data
     // MJ TEMP: don't use this field for now
     //StrVecType AddressesToPublish;
 
-    /*! Timestamp of last sampled data */
+    //! Timestamp of last sampled data
     double LastSamplingTick;
 
-    /*! Monitor type: individual monitor (for passive filtering) or monitor attached 
-        to filter (for active filtering) */
-    bool AttachedToActiveFilter; // MJTEMP: is this used????
+    //! Monitor type
+    /*! Either individual monitor (for passive filtering) or monitor attached 
+        to filter (for active filtering)
+    */
+    bool AttachedToActiveFilter; // MJTEMP: is this being used????
 
-    /*! Initialize internal variables */
+    //! Initialize internal variables
     void Initialize(void);
 
 public:
+    //! Default constructor
     Monitor();
+    //! Constructor with explicit arguments
     Monitor(const TargetType target, 
             EventLocationBase * locationId,
             const StateType state,
             const OutputType outputType,
             const SamplingRateType samplingRate = 0, // unsigned int type (Hz)
             double lastSamplingTick = 0.0);
+    //! Constructor using instance of JSON structure
+    Monitor(const JSON::JSONVALUE & jsonNode);
+    //! Constructor using JSON string.
+    /*! Internally calls constructor with JSON structure */
+    //Monitor(const std::string & jsonMonitorSpecString);
+    //! Destructor
     virtual ~Monitor();
 
-    /*! Placeholders for samples */
+    //! Placeholders for predefined monitoring targets
     struct {
         double Period;
         double ExecTimeUser;
         double ExecTimeTotal;
     } Samples;
 
-    /*! Assignment operator overloading */
+    //! Assignment operator overloading
     //Monitor & operator=(const Monitor & rhs);
 
-    /*! Returns numeric unique id (unique within process) */
+    //! Returns numeric unique id (unique within process)
+    // TODO
 
-    /*! Returns string representation of unique id of monitoring target.
-        Used to check duplicate monitoring target with the same monitoring
-        target type. */
+    //! Returns string representation of unique id of monitoring target.
+    /*! This is used to check duplicate monitoring target with the same monitoring
+        target type.
+    */
     const std::string GetUIDAsString(void) const;
 
-    /*! Returns numerical representation of unique id of monitoring target */
+    //! Returns numerical representation of unique id of monitoring target
     UIDType GetUIDAsNumber(void) const;
 
-    /*! Check if it is necessary to sample new data */
+    //! Check if it is necessary to sample new data
     bool IsSamplingNecessary(double currentTick) const;
 
-    /*! Update last sampling timestamp */
+    //! Update last sampling timestamp
     inline void UpdateLastSamplingTick(double tick) { LastSamplingTick = tick; }
 
-    //
-    //  Getters
-    //
+    /*! \addtogroup Getters
+        @{
+     */
     inline bool IsActive(void) const { return (State == STATE_ON); }
     inline bool IsStream(void) const { return (Output == OUTPUT_STREAM); }
     inline bool IsEvent(void) const  { return (Output == OUTPUT_EVENT); }
@@ -144,10 +175,11 @@ public:
     inline OutputType         GetOutputType(void) const     { return Output; }
     inline SamplingRateType   GetSamplingRate(void) const   { return SamplingRate; }
     inline SamplingPeriodType GetSamplingPeriod(void) const { return (1.0 / (SamplingPeriodType) SamplingRate); }
+    /*! @} */
 
-    //
-    //  Setters
-    //
+    /*! \addtogroup Setters
+        @{
+     */
     inline void SetTargetType(const TargetType target)       { Target = target; }
     inline void SetLocationID(EventLocationBase * locationId){ LocationID = locationId; }
     inline void SetState(const StateType state)              { State = state; }
@@ -156,27 +188,32 @@ public:
     inline void SetAttachedToActiveFilter(bool attachedToActiveFilter) {
         AttachedToActiveFilter = attachedToActiveFilter;
     }
+    /*! @} */
 
-    //
-    //  Misc. Getters
-    //
-    /*! Return string that corresponds to the monitor target */
+    /*! \addtogroup Misc. Getters
+        @{
+     */
+    //! Return string that corresponds to the monitor target
     static const std::string GetTargetTypeString(const TargetType type);
-    /*! Return target type from string */
+    //! Return target type from string
     static TargetType GetTargetTypeFromString(const std::string & str);
 
-    /*! Return string that corresponds to the monitor state */
+    //! Return string that corresponds to the monitor state
     static const std::string GetStateTypeString(const StateType type);
-    /*! Return target type from string */
+    //! Return state type from string
     static StateType GetStateTypeFromString(const std::string & str);
 
-    /*! Return string that corresponds to the monitor output */
+    //! Return string that corresponds to the monitor output
     static const std::string GetOutputTypeString(const OutputType output);
-    /*! Return output type from string */
+    //! Return output type from string
     static OutputType GetOutputTypeFromString(const std::string & str);
 
-    /*! For human-readable logging and debugging */
-    virtual void ToStream(std::ostream & outputStream) const;
+    //! For human-readable logging and debugging
+    /*! \param outputStream output stream
+        \param includeLocation target location is printed if yes (default: yes)
+    */
+    virtual void ToStream(std::ostream & outputStream, bool includeLocation = true) const;
+    /*! @} */
 };
 
 inline std::ostream & operator << (std::ostream & outputStream, const Monitor & monitor) {
