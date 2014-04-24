6b940a520d7635e3b897f091cf59abe99e92284a
Min Yang Jung <myj@jhu.edu>
2014-04-24 17:56:40 -0400

    updated file headers

--- libs/db/MongoDB.h
+++ libs/db/MongoDB.h
@@ -1,17 +1,16 @@
-/*
-
-  Safety Framework for Component-based Robotics
-
-  Created on: August 14, 2012
-
-  Copyright (C) 2012-3 Min Yang Jung, Peter Kazanzides
-
-  Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE_1_0.txt or copy at
-  http://www.boost.org/LICENSE_1_0.txt)
-
-*/
-
+//------------------------------------------------------------------------
+//
+// CASROS: Component-based Architecture for Safe Robotic Systems
+//
+// Copyright (C) 2012-2014 Min Yang Jung and Peter Kazanzides
+//
+//------------------------------------------------------------------------
+//
+// Created on   : Aug 14, 2012
+// Last revision: Apr 24, 2014
+// Author       : Min Yang Jung (myj@jhu.edu)
+// Github       : https://github.com/minyang/casros
+//
 #ifndef _MongoDB_h
 #define _MongoDB_h
 
