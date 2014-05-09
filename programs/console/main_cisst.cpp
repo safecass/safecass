41b252a4d8c53df45c764e78c1cd6fc95066a598
Min Yang Jung <myj@jhu.edu>
2014-05-09 10:10:29 -0400

    programs/console: integrated cisst, added publisher/subscriber

--- programs/console/main_cisst.cpp
+++ programs/console/main_cisst.cpp
@@ -11,13 +11,106 @@
 // Author       : Min Yang Jung (myj@jhu.edu)
 // Github       : https://github.com/minyang/casros
 //
-
+#include "config.h"
+#include "publisher.h"
+#include "subscriber.h"
+#include "dict.h"
 // Header from the Clipo project; See CMakeLists.txt for more details.
 #include "command_line_interpreter.hpp"
+
+#include <cisstMultiTask/mtsManagerLocal.h>
 
 namespace po = boost::program_options;
 
 typedef std::vector<std::string> StrVec;
+
+class ConsoleSubscriberCallback : public SF::SFCallback {
+public:
+    ConsoleSubscriberCallback() {}
+    ~ConsoleSubscriberCallback() {}
+
+    void Callback(const std::string & json) {
+        std::cout << "SUBSCRIBER: " << json << std::endl;
+    }
+};
+
+class Communicator {
+public:
+    typedef struct {
+        osaThread       Thread;
+        osaThreadSignal ThreadEventBegin;
+        osaThreadSignal ThreadEventEnd;
+        bool            Running;
+    } InternalThreadType;
+    
+    /*! Ice publisher and subscriber */
+    SF::Publisher *  Publisher;
+    SF::Subscriber * Subscriber;
+
+    /*! Callback for subscriber */
+    ConsoleSubscriberCallback * SubscriberCallback;
+
+    InternalThreadType ThreadSubscriber;
+
+    Communicator(void) {
+        //Publisher = new SF::Publisher(SF::Dict::TopicNames::data);
+        Publisher = new SF::Publisher(SF::Dict::TopicNames::control);
+        if (!Publisher->Startup()) {
+            std::stringstream ss;
+            ss << "Communicator: Failed to initialize publisher for topic \""
+                << SF::Dict::TopicNames::data << "\"";
+            cmnThrow(ss.str());
+        }
+
+        SubscriberCallback = new ConsoleSubscriberCallback;
+        Subscriber = new SF::Subscriber(SF::Dict::TopicNames::control, SubscriberCallback);
+        ThreadSubscriber.Thread.Create<Communicator, unsigned int>(this, &Communicator::RunSubscriber, 0);
+        ThreadSubscriber.ThreadEventBegin.Wait();
+    }
+
+    ~Communicator() {
+        if (Subscriber && ThreadSubscriber.Running) {
+            ThreadSubscriber.Running = false;
+            // Terminating subscriber needs to call shutdown() on the Ice communicator
+            Subscriber->Stop();
+            ThreadSubscriber.ThreadEventEnd.Wait();
+
+            delete Subscriber;
+            Subscriber = 0;
+            // SubscriberCallback is deleted by Subscriber's destructor.
+        }
+        delete Publisher;
+    }
+
+    void * RunSubscriber(unsigned int CMN_UNUSED(arg))
+    {
+        ThreadSubscriber.Running = true;
+
+        ThreadSubscriber.ThreadEventBegin.Raise();
+
+        try {
+            Subscriber->Startup();
+            while (ThreadSubscriber.Running) {
+                Subscriber->Run();
+            }
+        } catch (const Ice::InitializationException & e) {
+            CMN_LOG_RUN_ERROR << "Communicator::RunSubscriber: ice init failed: " << e.what() << std::endl;
+        } catch (const Ice::AlreadyRegisteredException & e) {
+            CMN_LOG_RUN_ERROR << "Communicator::RunSubscriber: ice init failed: " << e.what() << std::endl;
+        } catch (const std::exception & e) {
+            CMN_LOG_RUN_ERROR << "Communicator::RunSubscriber: exception: " << e.what() << std::endl;
+        }
+
+        ThreadSubscriber.ThreadEventEnd.Raise();
+
+        return 0;
+    }
+};
+
+// cisst Component Manager
+mtsManagerLocal * componentManager = 0;
+// Ice communicator instances
+Communicator * comm = 0;
 
 //------------------------------------------------------------ 
 //  help
@@ -27,9 +120,9 @@ void handler_help(const std::vector<std::string> &)
     std::stringstream ss;
 
 #define NL << std::endl
-    ss << "CASROS: Component-based Architecture for Safe Robotic Systems" NL NL
-       << "console [command]"NL NL
-       << "Supported commands:"NL
+    ss << "CASROS console utility" NL NL
+       << "USAGE: console [command]"NL NL
+       << "commands:"NL
        << "    help   : "NL
        << "    filter : "NL
        << "    state  : "NL
@@ -68,14 +161,59 @@ void handler_state(const std::vector<std::string> &parameters)
 //------------------------------------------------------------ 
 void handler_exit(const std::vector<std::string> &)
 {
+    delete comm;
+
+    componentManager->KillAll();
+    componentManager->WaitForStateAll(mtsComponentState::FINISHED, 2.0 * cmn_s);
+    componentManager->Cleanup();
+
     exit(0);
 }
 
+//------------------------------------------------------------ 
+//  main
+//------------------------------------------------------------ 
 int main(int argc, char **argv)
 {
     // TODO: if argc = 1, interactive mode; argc >= 2, one-time execution of input commands
     // TODO: connect to icebox
 
+#if SF_USE_G2LOG
+    // Logger setup
+    g2LogWorker logger(argv[0], "./");
+    g2::initializeLogging(&logger);
+    std::cout << "Log file: \"" << logger.logFileName() << "\"\n" << std::endl;
+#endif
+
+    // cisst logger setup
+    cmnLogger::SetMask(CMN_LOG_ALLOW_ALL);
+    cmnLogger::SetMaskFunction(CMN_LOG_ALLOW_ALL);
+    cmnLogger::SetMaskDefaultLog(CMN_LOG_ALLOW_ALL);
+    cmnLogger::AddChannel(std::cout, CMN_LOG_ALLOW_ERRORS_AND_WARNINGS);
+    //cmnLogger::AddChannel(std::cout, CMN_LOG_ALLOW_ALL);
+    //cmnLogger::SetMaskClassMatching("mts", CMN_LOG_ALLOW_ALL);
+
+    // TODO: add option to enable/disable SC
+    mtsManagerLocal::InstallSafetyCoordinator();
+
+    // Get local component manager instance
+    try {
+        componentManager = mtsManagerLocal::GetInstance();
+    } catch (...) {
+        CMN_LOG_INIT_ERROR << "Failed to initialize local component manager" << std::endl;
+        return 1;
+    }
+
+    // Create publisher and subscriber
+    comm = new Communicator;
+
+    componentManager->CreateAll();
+    componentManager->WaitForStateAll(mtsComponentState::READY);
+
+    componentManager->StartAll();
+    componentManager->WaitForStateAll(mtsComponentState::ACTIVE);
+
+    // Instantiate interpreter
     boost::cli::commands_description desc;
     desc.add_options()
         ("help",   po::value<StrVec>()->zero_tokens()->notifier(boost::bind(&handler_help, _1)))
@@ -87,4 +225,6 @@ int main(int argc, char **argv)
 
     boost::cli::command_line_interpreter cli(desc, ">");
     cli.interpret(std::cin);
+
+    return 0;
 }
