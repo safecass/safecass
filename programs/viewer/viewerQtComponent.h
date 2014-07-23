2f7cb5dad4cbbf1d13f59ebc6af729199af83273
Min Yang Jung <myj@jhu.edu>
2014-07-23 11:21:55 -0400

    adding copyright header to state viewer

--- programs/viewer/viewerQtComponent.h
+++ programs/viewer/viewerQtComponent.h
@@ -1,3 +1,16 @@
+//------------------------------------------------------------------------
+//
+// CASROS: Component-based Architecture for Safe Robotic Systems
+//
+// Copyright (C) 2012-2014 Min Yang Jung and Peter Kazanzides
+//
+//------------------------------------------------------------------------
+//
+// Created on   : Jul 14, 2014
+// Last revision: Jul 23, 2014
+// Author       : Min Yang Jung (myj@jhu.edu)
+// Github       : https://github.com/minyang/casros
+//
 #ifndef _viewerQtComponent_h
 #define _viewerQtComponent_h
 
