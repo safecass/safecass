e8f7182f8c4232f64abafd93097fbdd778fe4ba5
Min Yang Jung <myj@jhu.edu>
2015-10-18 10:36:28 -0400

    local change back up (cmake clean up work-in-progress)

---
+++ cmake/utils.cmake
@@ -0,0 +1,54 @@
+#-----------------------------------------------------------------------------------
+#
+# SAFECASS: Safety Architecture For Engineering Computer-Assisted Surgical Systems
+#
+# Copyright (C) 2012-2015 Min Yang Jung and Peter Kazanzides
+#
+#-----------------------------------------------------------------------------------
+#
+# Created on   : Oct 18, 2015
+# Last revision: Oct 18, 2015
+# Author       : Min Yang Jung (myj@jhu.edu)
+# URL          : https://github.com/safecass/safecass
+#
+
+#
+# Determine OS
+#
+set (SC_ON_MAC     FALSE)
+set (SC_ON_WINDOWS FALSE)
+set (SC_ON_LINUX   FALSE)
+
+# Mac/Apple
+if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
+  set (SC_ON_MAC TRUE)
+  message ("OS: Apple/Mac detected")
+elseif (UNIX)
+  set (SC_ON_LINUX TRUE)
+  message ("OS: Linux detected")
+elseif (WIN32)
+  set (SC_ON_WINDOWS TRUE)
+  message ("OS: Windows detected")
+endif()
+
+if (SC_ON_MAC)
+elseif (SC_ON_WINDOWS)
+elseif (SC_ON_LINUX)
+endif()
+
+#
+# Determine compiler
+#
+set (SC_COMPILER "")
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  set (SC_COMPILER "Clang")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  set (SC_COMPILER "GCC")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  set (SC_COMPILER "MSVC")
+else()
+  message(FATAL_ERROR "Failed to determine compiler")
+endif()
+
+message ("Compiler: ${SC_COMPILER} detected")
+
