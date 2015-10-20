27db484c96b60c5a56d7eeee4bbd182ab6da8c80
Min Yang Jung <myj@jhu.edu>
2015-10-20 08:29:41 -0400

    udpates to travis.sh

--- travis.sh
+++ travis.sh
@@ -3,11 +3,12 @@ set -evx
 env | sort
 
 mkdir build || true
+cd build
 #mkdir build/$SAFECASS_TARGET || true
-mkdir build/$SAFECASS_TARGET || true
-cd build/$SAFECASS_TARGET
+#cd build/$SAFECASS_TARGET
 cmake -DSAFECASS_BUILD_UNIT_TEST=ON \
       -DSAFECASS_USE_G2LOG=ON \
-      ../../$SAFECASS_TARGET
+      ..
+      #../$SAFECASS_TARGET
 make
 make test
