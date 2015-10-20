bcff2e55f522adebe33d80e2bba525fab098dddd
Min Yang Jung <myj@jhu.edu>
2015-10-20 08:21:28 -0400

    build chain update (work in progress)

--- travis.sh
+++ travis.sh
@@ -7,7 +7,6 @@ mkdir build/$SAFECASS_TARGET || true
 cd build/$SAFECASS_TARGET
 cmake -DSAFECASS_BUILD_UNIT_TEST=ON \
       -DSAFECASS_USE_G2LOG=ON \
-      #-DCMAKE_CXX_FLAGS=$CXX_FLAGS \
-      ../../$SAFECASS_TARGET
+      ../$SAFECASS_TARGET
 make
 make test
