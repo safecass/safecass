34a3eccc61e17762ca80b67dd6042304ab7ab4e3
Min Yang Jung <myj@jhu.edu>
2015-10-21 10:32:10 -0400

    updates

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,7 +10,7 @@ cd build
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export CMAKE_BIN="../cmake332/bin/cmake";
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    export CMAKE_BIN="../cmake332/cmake-3.3.2-Darwin-x86_64/cmake-3.3.2-Darwin-x86_64/CMake.app/Contents/bin";
+    export CMAKE_BIN="../cmake332/cmake-3.3.2-Darwin-x86_64/cmake-3.3.2-Darwin-x86_64/CMake.app/Contents/bin/cmake";
 else
     # fallback
     export CMAKE_BIN="cmake";
