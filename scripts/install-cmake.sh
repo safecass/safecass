b85287aa03a4640713d219534842c879f8895c63
Min Yang Jung <myj@jhu.edu>
2015-10-21 00:23:00 -0400

    Updated travis build chain to support OS X

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -4,30 +4,29 @@ set -ex
 # TEMP
 export TRAVIS_OS_NAME="osx";
 
-
 # Detect travis OS and set cmake package file name to download
-travis_on_linux=true
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     export CMAKE_PACKAGE_NAME="cmake-3.3.2-Darwin-x86_64.tar.gz";
-    travis_on_linux=false
 elif if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export CMAKE_PACKAGE_NAME="cmake-3.3.2-Linux-x86_64.sh";
 fi
-
-echo CMake package to download (${TRAVIS_OS_NAME}): ${CMAKE_PACKAGE_NAME}
+ 
+echo ${TRAVIS_OS_NAME}
+echo CMake package to download: ${CMAKE_PACKAGE_NAME}
 
 # Download cmake pacakge (minimum 3.0 due to external project support)
 wget --no-check-certificate https://cmake.org/files/v3.3/${CMAKE_PACKAGE_NAME}
 
 # For Linux
-if [ "$travis_on_linux" = true ]; then
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   chmod +x ${CMAKE_PACKAGE_NAME}
   mkdir cmake332
   cd cmake332
   ../${CMAKE_PACKAGE_NAME} --skip-license
   cd ..
+fi
 # For OS X
-else
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   mkdir cmake332
   cd cmake332
   tar -zxf ../${CMAKE_PACKAGE_NAME}
