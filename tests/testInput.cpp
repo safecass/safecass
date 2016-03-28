bacbc58868dd012ec0ca120e7ae715a35be6d453
Min Yang Jung <myj@jhu.edu>
2016-03-28 00:14:00 -0400

    Updates for Eigen3

--- tests/testInput.cpp
+++ tests/testInput.cpp
@@ -14,6 +14,7 @@
 #include "gtest/gtest.h"
 
 #include "historyBuffer.h"
+#include "paramEigen.h"
 
 using namespace SC;
 
@@ -114,4 +115,35 @@ TEST(HistoryBuffer, spsc_array_basic)
 // Tests accessors that HistoryBuffer provides
 TEST(HistoryBuffer, spsc_array_accessors)
 {
+}
+
+// Tests Eigen parameter handling
+TEST(HistoryBuffer, eigen_params)
+{
+#if 1
+    Minimal<int>A;
+    Minimal<short>B;
+    Minimal<float>E(1.4f);
+    Minimal<std::vector<int> >X;
+    // calling setZero for a vector...?
+    X.setZero();
+
+    Minimal<Eigen::Vector2f>C;
+    Minimal<Eigen::Vector2f>D(Eigen::Vector2f::Random());
+    Minimal<Eigen::Array2i>F(Eigen::Array2i(2,8));
+    C = D;
+
+    std::cerr << " E " << E << " C " << C << " A " << A.val << " B " << B.val << "\n";
+
+    A.increase();
+    E.decreace();
+    A.increase();
+    C.setZero();
+    C.increase();
+    B.increase();
+    B.increase();
+    B.increase();
+
+    std::cerr << " E " << E << " C " << C << " A " << A.val << " B " << B.val << "\n";
+#endif
 }
