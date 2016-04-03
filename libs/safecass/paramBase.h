0ec769209c1dc63b72b586ca2921a5d0a40d7489
Min Yang Jung <myj@jhu.edu>
2016-04-03 15:08:55 -0400

    Created ParamBase class as the base class of parameter types

---
+++ libs/safecass/paramBase.h
@@ -0,0 +1,56 @@
+//-----------------------------------------------------------------------------------
+//
+// SAFECASS: Safety Architecture For Engineering Computer-Assisted Surgical Systems
+//
+// Copyright (C) 2012-2016 Min Yang Jung and Peter Kazanzides
+//
+//-----------------------------------------------------------------------------------
+//
+// Created on   : Apr 2, 2016
+// Last revision: Apr 3, 2016
+// Author       : Min Yang Jung <myj@jhu.edu>
+// Github       : https://github.com/safecass/safecass
+//
+
+/*!
+    This file implements the base class of parameter types that safecass uses.
+*/
+
+#ifndef _ParamBase_h
+#define _ParamBase_h
+
+namespace SC {
+
+class ParamBase
+{
+protected:
+    bool Valid;
+
+public:
+    ParamBase(void): Valid(false) {}
+    virtual ~ParamBase() {}
+
+    //! Accessors for Valid property
+    inline bool IsValid(void) const {
+        return Valid;
+    }
+    void SetValid(bool isValid = true) {
+        Valid = isValid;
+    }
+
+    virtual void ToStream(std::ostream & os) const {
+        os << (Valid ? "valid" : "invalid");
+    }
+
+    virtual ParamBase * Clone(void) const = 0;
+};
+
+inline std::ostream & operator << (std::ostream & os, const ParamBase & param)
+{
+    param.ToStream(os);
+    return os;
+}
+
+}; // SC
+
+#endif // _ParamBase_h
