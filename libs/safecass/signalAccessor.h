9493a6a7f0fe26daee51fdb89a02e1aaa93e9566
Min Yang Jung <myj@jhu.edu>
2016-04-26 00:08:14 -0400

    Proper handling of assignment operator in GetNewValue()

--- libs/safecass/signalAccessor.h
+++ libs/safecass/signalAccessor.h
@@ -111,7 +111,26 @@ public:
             arg.SetValid(false);
             return;
         }
-        arg = Container->back();
+
+        // circular_buffer memory layout:
+        //
+        // boost::circular_buffer<int> cb(3);
+        // cb.push_back(1);
+        // cb.push_back(2);
+        // cb.push_back(3);
+        // cb.push_back(4);
+        //
+        // assert(cb[0] == 2);
+        // assert(cb[1] == 3);
+        // assert(cb[2] == 4);
+        //
+        // assert(*cb.begin() == 2);
+        // assert(cb.front() == 2);
+        // assert(cb.back() == 4);
+
+        ValueType * pArg = dynamic_cast<ValueType *>(&arg);
+        SCASSERT(pArg);
+        *pArg = Container->back();
     }
 
     //
