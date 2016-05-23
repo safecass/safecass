b0d2706e8626b3a62596ac337b5405bcf68605f7
Min Yang Jung <myj@jhu.edu>
2016-05-23 01:18:29 -0400

    GCM: Added GraphViz export feature (work-in-progress)

--- libs/safecass/code/gcm.cpp
+++ libs/safecass/code/gcm.cpp
@@ -56,13 +56,8 @@ void GCM::InitGraph(void)
     }
 
     // Add vertices
-    GCM::VertexDescriptor s_ext = boost::add_vertex(Graph);
     GCM::VertexDescriptor s_A   = boost::add_vertex(Graph);
     GCM::VertexDescriptor s_F   = boost::add_vertex(Graph);
-
-    Graph[s_ext].Name = "s_ext";
-    Graph[s_ext].StateType = GCM::STATE_EXTENDED;
-    Graph[s_ext].SM = new StateMachine(ComponentName);
 
     Graph[s_A].Name = "s_A";
     Graph[s_A].StateType = GCM::STATE_APPLICATION;
@@ -75,10 +70,53 @@ void GCM::InitGraph(void)
     // Add edges between vertices
     EdgeDescriptor e;
     bool added;
-    boost::tie(e, added) = boost::add_edge(s_A, s_ext, EdgeProperty(EDGE_DEPENDENCY), Graph);
-    SCASSERT(added);
-    boost::tie(e, added) = boost::add_edge(s_F, s_ext, EdgeProperty(EDGE_DEPENDENCY), Graph);
-    SCASSERT(added);
+    // boost::tie(e, added) = boost::add_edge(s_A, s_ext, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    // SCASSERT(added);
+
+    // TEMP Following nodes and edges are added only for testing purpose
+    GCM::VertexDescriptor s_R1 = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_R2 = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_R3 = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_R4 = boost::add_vertex(Graph);
+
+    GCM::VertexDescriptor s_P1 = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_P2 = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_P3 = boost::add_vertex(Graph);
+
+    GCM::VertexDescriptor s_P1_derived = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_P2_derived = boost::add_vertex(Graph);
+    GCM::VertexDescriptor s_P3_derived = boost::add_vertex(Graph);
+
+#define ADD_VERTEX(_v, _name, _type)\
+    Graph[_v].Name = _name;\
+    Graph[_v].StateType = _type;\
+    Graph[_v].SM = new StateMachine(ComponentName);
+
+    ADD_VERTEX(s_R1, "required1", GCM::STATE_REQUIRED);
+    ADD_VERTEX(s_R2, "required2", GCM::STATE_REQUIRED);
+    ADD_VERTEX(s_R3, "required3", GCM::STATE_REQUIRED);
+    ADD_VERTEX(s_R4, "required4", GCM::STATE_REQUIRED);
+
+    ADD_VERTEX(s_P1, "provided1", GCM::STATE_PROVIDED);
+    ADD_VERTEX(s_P2, "provided2", GCM::STATE_PROVIDED);
+    ADD_VERTEX(s_P3, "provided3", GCM::STATE_PROVIDED);
+
+    ADD_VERTEX(s_P1_derived, "~provided1", GCM::STATE_SERVICE);
+    ADD_VERTEX(s_P2_derived, "~provided2", GCM::STATE_SERVICE);
+    ADD_VERTEX(s_P3_derived, "~provided3", GCM::STATE_SERVICE);
+
+    boost::add_edge(s_P1, s_P1_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_P2, s_P2_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_P3, s_P3_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_F, s_P1_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_F, s_P2_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_A, s_P2_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+    boost::add_edge(s_A, s_P3_derived, EdgeProperty(EDGE_DEPENDENCY), Graph);
+
+    boost::add_edge(s_R1, s_P1, EdgeProperty(EDGE_ERROR_PROP), Graph);
+    boost::add_edge(s_R1, s_P2, EdgeProperty(EDGE_ERROR_PROP), Graph);
+    boost::add_edge(s_R2, s_P3, EdgeProperty(EDGE_ERROR_PROP), Graph);
+    boost::add_edge(s_R4, s_P2, EdgeProperty(EDGE_ERROR_PROP), Graph);
 }
 
 void GCM::ToStream(std::ostream & os, ExportFormatType format) const
@@ -133,15 +171,131 @@ void GCM::ToStream(std::ostream & os, ExportFormatType format) const
         }
     }
     else if (format == EXPORT_GRAPHVIZ) {
-        /*
-template < typename VertexAndEdgeListGraph, typename VertexPropertyWriter,
-           typename EdgePropertyWriter, typename GraphPropertyWriter >
-void
-write_graphviz(std::ostream& out, const VertexAndEdgeListGraph& g,
-               VertexPropertyWriter vpw, EdgePropertyWriter epw,
-               GraphPropertyWriter gpw);
-        */
-        boost::write_graphviz(os, Graph);//, make_label_writer(name));
+#if 0
+        boost::write_graphviz(os, Graph);
+        boost::write_graphviz(os, Graph,
+                              GCM::VertexPropertyWriter(Graph),
+                              GCM::EdgePropertyWriter(Graph),
+                              GCM::GraphPropertyWriter());
+#endif
+        // Graph
+        os << "digraph " << ComponentName << " {" << std::endl
+           << "node [shape=record];" << std::endl
+           << "edge [style=solid splines=curved];" << std::endl
+           << "rankdir=LR;" << std::endl;
+
+        std::stringstream ss_required, ss_provided, ss_provided_service, ss_others;
+        size_t n_required = 0, n_provided = 0, n_provided_service = 0;
+
+        std::vector<size_t> vecRequired, vecProvided, vecProvidedService;
+
+        ss_required << "required [label=\"";
+        ss_provided << "provided [label=\"";
+        ss_provided_service << "provided_service [label=\"";
+
+        VertexIter itVertex, itVertexEnd;
+        boost::tie(itVertex, itVertexEnd) = vertices(Graph);
+        for (; itVertex != itVertexEnd; ++itVertex) {
+            switch (Graph[*itVertex].StateType) {
+            case STATE_REQUIRED:
+                if (n_required > 0)
+                    ss_required << "|";
+                ss_required << "<s" << n_required++ << "> " << Graph[*itVertex].Name;
+                vecRequired.push_back(*itVertex);
+                break;
+
+            case STATE_PROVIDED:
+                if (n_provided > 0)
+                    ss_provided << "|";
+                ss_provided << "<s" << n_provided++ << "> " << Graph[*itVertex].Name;
+                vecProvided.push_back(*itVertex);
+                break;
+
+            case STATE_SERVICE:
+                if (n_provided_service > 0)
+                    ss_provided_service << "|";
+                ss_provided_service << "<s" << n_provided_service++ << "> " << Graph[*itVertex].Name;
+                vecProvidedService.push_back(*itVertex);
+                break;
+
+            case STATE_FRAMEWORK:
+            case STATE_APPLICATION:
+                ss_others << Graph[*itVertex].Name << " [label=<s<font point-size=10>F</font>>, fillcolor=yellow, style=\"rounded,filled\", shape=rounded];" << std::endl;
+                break;
+
+            default:
+                ;
+            }
+            // TODO add node color
+
+            // os << *itVertex << ": " << Graph[*itVertex].Name << " (" << GetStateTypeString(Graph[*itVertex].StateType)
+                // << "), State: " << State::GetStringState(Graph[*itVertex].SM->GetCurrentState()) << std::endl;
+        }
+        ss_required << "\"];";
+        ss_provided << "\"];";
+        //ss_provided_service << "\", style=filled, fillcolor=gray];";
+        ss_provided_service << "\", style=dashed];";
+
+        // FIXME check if n_required > 0 (??)
+        os << ss_required.str() << std::endl;
+        os << ss_provided.str() << std::endl;
+        os << ss_provided_service.str() << std::endl;
+        os << ss_others.str() << std::endl;
+        // for testing
+        //os << "title [label=<Birth of George Washington<BR /> <FONT POINT-SIZE=\"10\">See also: American Revolution</FONT>>];" << std::endl;
+
+        // Print list of edges
+        std::stringstream edges;
+
+        const std::string arrowStyle = "[arrowhead=vee]";
+
+        // 1. Outgoing edges from required interface state to provided interface state
+        for (size_t i = 0; i < vecRequired.size(); ++i) {
+            // Outgoing edges from source of which index is vecRequired[i] in Graph
+            OutEdgeIter itEdgeOut, itEdgeOutEnd;
+            boost::tie(itEdgeOut, itEdgeOutEnd) = boost::out_edges(vecRequired[i], Graph);
+            for (; itEdgeOut != itEdgeOutEnd; ++itEdgeOut) {
+                // Target
+                edges << "required:s" << i << " -> ";
+                // Look for target vertex in provided interface state vertices using its index
+                const size_t idx = boost::target(*itEdgeOut, Graph);
+                for (size_t j = 0; j < vecProvided.size(); ++j) {
+                    if (vecProvided[j] == idx) {
+                        edges << "provided:s" << j << " " << arrowStyle << ";" << std::endl;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // 2. Incoming edges to provided interface service state
+        for (size_t i = 0; i < vecProvidedService.size(); ++i) {
+            InEdgeIter itEdgeIn, itEdgeInEnd;
+            boost::tie(itEdgeIn, itEdgeInEnd) = boost::in_edges(vecProvidedService[i], Graph);
+            for (; itEdgeIn != itEdgeInEnd; ++itEdgeIn) {
+                // Get source vertex
+                const size_t idx = boost::source(*itEdgeIn, Graph);
+                // Check if source is provided interface. If yes, search for vecProvided
+                // for its index.  If source is either s_A or s_F, edge needs to be defined
+                // differently.
+                if (Graph[idx].StateType == STATE_PROVIDED) {
+                    for (size_t j = 0; j < vecProvided.size(); ++j) {
+                        if (vecProvided[j] == idx) {
+                            edges << "provided:s" << j;
+                            break;
+                        }
+                    }
+                } else {
+                    edges << Graph[idx].Name;
+                }
+                edges << " -> " << "provided_service:s" << i << " " << arrowStyle << ";" << std::endl;
+            }
+        }
+
+        // export edges
+        os << edges.str() << std::endl;
+
+        os << "}" << std::endl;
     }
 }
 
@@ -149,7 +303,6 @@ std::string GCM::GetStateTypeString(StateTypes type)
 {
     switch (type) {
     case STATE_INVALID:     return "INVALID_STATE";
-    case STATE_EXTENDED:    return "EXTENDED_STATE";
     case STATE_FRAMEWORK:   return "FRAMEWORK_STATE";
     case STATE_APPLICATION: return "APPLICATION_STATE";
     case STATE_REQUIRED:    return "REQUIRED_STATE";
--- libs/safecass/gcm.h
+++ libs/safecass/gcm.h
@@ -65,55 +65,9 @@ public:
     */
     //typedef std::map<std::string, StateMachine *> InterfaceStateMachinesType;
 
-    //! Typedef of complete set of state machines representing GCM component status
-    // => Replaced by graph
-#if 0
-    /*!
-        State machines for component states (Framework and Application states) always
-        exist, whereas state machines for interface states are dynamically created only
-        when interfaces are registered to SAFECASS.
-    */
-    typedef struct {
-        StateMachine Framework;                      /*!< Component state in system view */
-        StateMachine Application;                    /*!< Component state in application View */
-        InterfaceStateMachinesType RequiredInterfaces; /*!< Collection of required interface states */
-        InterfaceStateMachinesType ProvidedInterfaces; /*!< Collection of provided interface states */
-    } StateMachinesType;
-#endif
-
 protected:
-    //! Typedef of service state dependency specification
-    /*!
-        1) Front-end: JSON
-
-        "service" : [
-            {   // name of provided interface
-                "name" : "Robot",
-                // service state dependency information
-                "dependency" : {
-                    "s_R" : [ "ForceSensor", "AMCInterface", "PendantRequired" ],
-                    "s_A" : true,
-                    "s_F" : true
-                }
-            },
-            {   "name" : "prmRobot",
-                "dependency" : {
-                    "s_R" : [ "ForceSensor", "AMCInterface", "PendantRequired" ],
-                    "s_A" : true,
-                    "s_F" : true
-                }
-            }
-        ]
-
-        2) Back-end: boost::adjacency_list of boost.graph
-
-            http://www.boost.org/doc/libs/1_60_0/libs/graph/doc/using_adjacency_list.html
-    */
-    //typedef std::map<std::string, StrVecType *> ServiceStateDependencyInfoType;
-
     typedef enum {
         STATE_INVALID,
-        STATE_EXTENDED,    // Extended state
         STATE_FRAMEWORK,   // Framework state
         STATE_APPLICATION, // Application state
         STATE_REQUIRED,    // Required interface state
@@ -126,6 +80,12 @@ protected:
         EDGE_ERROR_PROP  // Edge representing error propagation
     } EdgeTypes;
 
+    //
+    // Graph containing information about state dependency and error propagation based on
+    // Boost Graph Library (BGL).
+    //
+    // References:
+    //
     // - BGL:
     //      http://www.boost.org/doc/libs/1_60_0/libs/graph/doc/table_of_contents.html
     // - Boost Property:
@@ -136,7 +96,7 @@ protected:
     //      http://www.boost.org/doc/libs/1_60_0/libs/graph/doc/using_adjacency_list.html#sec:adjacency-list-properties
     // - Property example:
     //      build/external_packages/boost/src/libs/graph/example/edge_property.cpp
-
+    //
     struct VertexProperty {
         std::string  Name;
         StateTypes   StateType;
@@ -162,23 +122,64 @@ protected:
     typedef boost::graph_traits<GraphType>::out_edge_iterator OutEdgeIter;
     typedef boost::graph_traits<GraphType>::in_edge_iterator  InEdgeIter;
 
+    //
+    // BGL property writers for GraphViz export feature
+    //
+    // A property writer for vertices or edges is a functor which can be called with two
+    // parameters: std::ostream and either a vertex or an edge descriptor.
+    // A property writer for graphs is a functor which is called with one parameter of type
+    // std::ostream and should print a series of graph properties.
+    //
+    // TO APPLY: cluster
+    // http://www.graphviz.org/Gallery/directed/cluster.html
+    //
+    // References:
+    //
+    // GraphViz Node Shapes: http://www.graphviz.org/doc/info/shapes.html
+    //
+
+#if 0
+    // Vertex property writer
+    class VertexPropertyWriter {
+    private:
+        const GraphType & g;
+    public:
+        VertexPropertyWriter(const GraphType & graph): g(graph) {}
+        template <class Vertex>
+        void operator()(std::ostream & os, const Vertex & v) const {
+            //os << "[label=\"" << g[v].Name << "\"]";
+            os << "[label=<" << g[v].Name << ">]";
+        }
+    };
+
+    // Edge property writer
+    class EdgePropertyWriter {
+    private:
+        const GraphType & g;
+    public:
+        EdgePropertyWriter(const GraphType & graph): g(graph) {}
+        template <class Edge>
+        void operator()(std::ostream & os, const Edge & v) const {
+            os << "[label=\"" << "FIXME" << "\"]";
+        }
+    };
+
+    // Graph property writer
+    class GraphPropertyWriter {
+    public:
+        void operator()(std::ostream & os) const {
+            //os << "graph [bgcolor=lightgrey]" << std::endl;
+            os << "node [shape=ellipse color=black]" << std::endl;
+            os << "edge [style=dashed splines=curved]" << std::endl;
+        }
+    };
+#endif
+
     //! Graph containing a set of GCM state machine objects, i.e., instances of StateMachine
     /*!
         Directed
     */
     GraphType Graph;
-
-    // This may not be required either (replaced by graph)
-#if 0
-    // typedef for connection information (to identify required interface)
-    typedef struct {
-        std::string SafetyCoordinatorName;
-        std::string ComponentName;
-        std::string RequiredInterfaceName;
-    } RequiredInterfaceInfoType;
-    typedef std::vector<RequiredInterfaceInfoType> ConnectionListType;
-    typedef std::map<std::string, ConnectionListType *> ConnectionsType;
-#endif
 
     //! Name of Coordinator instance that this GCM belongs to
     /*!
@@ -220,122 +221,6 @@ public:
     //! Returns name of component that this GCM represents
     inline const std::string & GetComponentName(void) const { return ComponentName; }
     /*! @} */
-
-#if 0
-protected:
-    /*! State machines */
-    StateMachinesType States;
-
-    // Connection information
-    // key: provided interface name, value: list of required interface information
-    ConnectionsType Connections;
-
-    /*! Service state dependency information */
-    // key: name of [ s_A, s_F, required interfaces ]
-    // value: a list of names of provided interfaces that are dependent on elements of name "key"
-    // Used to decide if service state should change when a state transition occurs.
-    ServiceStateDependencyInfoType ServiceStateDependencyInfo;
-    // key: name of provided interfaces
-    // value: a list of elements names on which a provided interface state depends
-    // Used to calculate service state of a provided interface
-    ServiceStateDependencyInfoType ServiceStateDependencyInfo2;
-
-    // Get json representation of service state change
-    void GetJSONForServiceStateChange(const std::string & providedInterfaceName, Json::Value & json);
-    // Get statemachine instance
-    const StateMachine * GetStateMachineComponent(ViewType view) const;
-    const StateMachine * GetStateMachineInterface(const std::string & name, InterfaceType type) const;
-
-    void PopulateStateUpdateJSON(const std::string & providedInterfaceName, Json::Value & json) const;
-
-private:
-    /*! Component associated with GCM has to be declared */
-    GCM(void);
-
-public:
-    /*! Constructor with owner.  GCM is initialized based on information about owner 
-        component specified. */
-    GCM(const std::string & coordinatorName, const std::string & componentName);
-
-    /*! Destructor */
-    virtual ~GCM(void);
-
-    /*! Add interface */
-    bool AddInterface(const std::string & name, InterfaceType type);
-    /*! Find interface */
-    bool FindInterface(const std::string & name, InterfaceType type) const;
-    /*! Remove interface */
-    bool RemoveInterface(const std::string & name, InterfaceType type);
-    // Add service state dependency information
-    void AddServiceStateDependency(const Json::Value & services);
-    bool AddServiceStateDependencyEntry(const std::string & providedInterfaceName,
-                                        const std::string & name);
-
-    // Process state transition event and returns transition and json object that contains 
-    // service state changes.
-    State::TransitionType ProcessStateTransition(State::StateMachineType type,
-                                                 Event & event,
-                                                 const std::string & interfaceName,
-                                                 Json::Value & json);
-
-    // Add connection information (about required interface) to provided interface
-    // This information is used to build up json for error propagation.
-    void AddConnection(const std::string & providedInterfaceName,
-                       const std::string & safetyCoordinatorName,
-                       const std::string & requiredComponentName,
-                       const std::string & requiredInterfaceName);
-
-    // Reset all states and events
-    void ResetStatesAndEvents(void);
-    // Reset states and events of particular type of state machine
-    void ResetStatesAndEvents(State::StateMachineType type);
-
-    // Extract state transition history from state machines and populate JSON object.
-    // baseId is used as the id of the first entry (state machine).
-    // Returns a number of state machines extracted.
-    unsigned int GetStateHistory(Json::Value & json, unsigned int baseId = 0);
-
-    //
-    // Getters
-    //
-    //! Returns names of interfaces
-    StrVecType GetNamesOfInterfaces(InterfaceType type) const;
-    void       GetNamesOfInterfaces(InterfaceType type, StrVecType & names) const;
-    //! Returns component state
-    State::StateType GetComponentState(ViewType view = SYSTEM_VIEW) const;
-    //! Returns component state with outstanding event.  
-    State::StateType GetComponentState(ViewType view, const Event* & e) const;
-    //! Returns interface state
-    State::StateType GetInterfaceState(const std::string & name, InterfaceType type) const;
-    //! Returns interface state with outstanding event
-    State::StateType GetInterfaceState(const std::string & name, InterfaceType type, const Event* & e) const;
-    //! Returns consolidated interface state
-    State::StateType GetInterfaceState(InterfaceType type) const;
-    State::StateType GetInterfaceState(InterfaceType type, const Event* & e) const;
-    //! Returns service state of provided interface and event information that caused service state transition
-    State::StateType GetServiceState(const std::string & providedInterfaceName, const Event * & event,
-                                     bool forErrorPropagation) const;
-    //
-    // Setters
-    //
-    // Install user-defined statemachine event handler
-    bool SetEventHandlerForComponent(GCM::ViewType view, StateEventHandler * handler);
-    bool SetEventHandlerForInterface(GCM::InterfaceType type, const std::string & interfaceName,
-                                     StateEventHandler * handler);
-
-    //
-    // Misc.
-    //
-    //! Human readable output of this class
-    void ToStream(std::ostream & outputStream) const;
-    // Print service state dependency table in tabular format
-    void PrintServiceStateDependencyTable(std::ostream & out, const std::string & prefix = "");
-    // Given a provided interface, print connection information
-    void PrintConnections(const std::string providedInterfaceName, std::ostream & out,
-                          const std::string & prefix = "");
-    // Print connection information for all provided interfaces
-    void PrintConnections(std::ostream & out, const std::string & prefix = "");
-#endif
 
     //! Human readable output of this class
     void ToStream(std::ostream & os = std::cout, ExportFormatType format = EXPORT_HUMAN_READABLE) const;
